59e932ddbd71d17af0cb9ce8da4b1c17b4cb930d
DhairyaPal6099 <dhairyapalofficial@gmail.com>
2025-09-30 15:31:27 -0400

    Added components to profile page

--- frontend/src/app/profile/page.tsx
+++ frontend/src/app/profile/page.tsx
@@ -1,8 +1,15 @@
 export default function Profile() {
     return (
         <>
-            <main className="flex min-h-screen flex-col items-center justify-between p-24">
-                <h1 className="text-4xl font-bold">Welcome to the Profile Page</h1>
+            <main className="flex flex-col p-5">
+                <h1 className="text-4xl font-bold">Profile</h1>
+                <div className="mt-5">
+                    <p className="text-lg mt-5">Name</p>
+                    <input type="text" className="mt-2 border p-2 rounded" defaultValue="JohnDoe" />
+                    <p className="text-lg mt-5">Email</p>
+                    <input type="email" className="mt-2 border p-2 rounded" defaultValue="johndoe@gmail.com" />
+                    <p className="text-lg mt-5">Instruments</p>
+                </div>
             </main>
         </>
     );
