8f31c30b8bc55afa765c16e03cde70481a784787
DhairyaPal6099 <dhairyapalofficial@gmail.com>
2025-10-06 10:09:52 -0400

    Removed placeholder text under Aesthetics to make space for profile picture display

--- frontend/src/app/profile/page.tsx
+++ frontend/src/app/profile/page.tsx
@@ -153,9 +153,6 @@ export default function Profile() {
               {/* AESTHETICS */}
               <div className="w-1/3 p-10">
                 <h1 className="text-4xl font-bold">Aesthetics</h1>
-                <p className="text-lg">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </p>
               </div>
 
               {/* MUSIC STUFF */}
