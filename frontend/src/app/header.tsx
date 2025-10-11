ccdf3b7cbb249b24f30df1c5bf4abdcaeef545dd
DhairyaPal6099 <dhairyapalofficial@gmail.com>
2025-10-10 23:24:14 -0400

    Changed header colors for all themes

--- frontend/src/app/header.tsx
+++ frontend/src/app/header.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Header() {
     return (
-        <header className="flex bg-theme text-theme items-center justify-between p-4">
+        <header className="flex bg-header text-header items-center justify-between p-4">
             <h2><Link href="/" className="text-primary hover:opacity-80 transition-opacity">(Logo) Musicer</Link></h2>
             <h2><Link href="/profile" className="text-primary hover:opacity-80 transition-opacity">(Profile photo)</Link></h2>
         </header>
