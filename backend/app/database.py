9bdc87bb6d1ebff92ba61c38af0a6a56a887e6ae
DhairyaPal6099 <dhairyapalofficial@gmail.com>
2025-10-11 21:56:50 -0400

    Added database connection code to database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -3,3 +3,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+DATABASE_URL = "postgresql://postgres:password@localhost:5432/musicer"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
