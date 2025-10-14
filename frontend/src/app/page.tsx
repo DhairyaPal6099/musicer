384b8a8c3cbf2f55d3bf5a3cf1f2c7581178088a
DhairyaPal6099 <dhairyapalofficial@gmail.com>
2025-10-14 10:55:09 -0400

    Added Home page greeting and footer

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,14 @@
 export default function Home() {
   return (
     <>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-theme text-theme transition-colors duration-300">
-        <h1 className="text-4xl font-bold text-primary">Welcome to the Home Page</h1>
+      <main className="flex min-h-screen flex-col justify-between pt-10 pl-10 bg-theme text-theme transition-colors duration-300">
+        <h1 className="text-4xl font-bold text-primary">Hey User!👋</h1>
+        {/* FOOTER */}
+        <footer className="py-8 text-gray-500 text-sm">
+          <center>
+            Built by Dhairya Pal © 2025
+          </center>
+        </footer>
       </main>
     </>
   );
