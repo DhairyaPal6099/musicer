59eb2f1d59c49e2e2730bc358847a266f6b5b94d
DhairyaPal6099 <dhairyapalofficial@gmail.com>
2025-10-14 14:49:23 -0400

    Added a todo.md to keep track of what's not done. Cleaned up unused code and UI a little bit

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Header from "../components/ui/header";
 import { Toaster } from "@/components/ui/sonner";
 import BackgroundVideo from "@/components/ui/BackgroundVideo";
 import HeaderWrapper from "@/components/headerwrapper";
---
+++ todo.md
@@ -0,0 +1,3 @@
+### Make use of the file variable in profilepicture.tsx (if it's useful) and store the pfp that the user uploads as a file somewhere because base64 strings are very long to be stored in db  
+### implement login authentication and then only allow user to go to /dashboard  
+### un-hardcode the dashboard page
